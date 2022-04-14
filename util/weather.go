82b30daa662ea6070747864ffbfe04bc89148c03
zg <zenggang666@gmail.com>
2022-04-14 09:28:00 +0800

    更新邮件样式

--- util/weather.go
+++ util/weather.go
@@ -92,8 +92,8 @@ type RspGetWeather struct {
 }
 
 const (
-	TD  = `<td style="padding:6px 10px 6px 10px">`
-	TD2 = `<td style="padding:2px 10px 2px 10px">`
+	Style1 = `style="padding:3px 10px"`
+	Style2 = `style="padding:0px 10px"`
 )
 
 // GetWeather 获取天气信息
@@ -123,23 +123,27 @@ func GetWeather() (string, error) {
 		return "", err
 	}
 	now := time.Now()
-	table := `<table>`
+	table := "<table>"
 	for k, item := range rsp.Data {
 		if k == 0 {
-			table += fmt.Sprintf("<tr>%v%v</td>%v%v</td>%v%v℃</td>%v%v</td></tr>\n", TD, now.Format("15:04"), TD, city, TD, item.Tem, TD, item.Wea)
-			table += fmt.Sprintf("<tr>%v%v</td>%v%v</td>%v%v~%v℃</td>%v%v</td></tr>\n", TD, item.Date[5:], TD, item.Week, TD, item.Tem2, item.Tem1, TD, item.Wea)
+			table += fmt.Sprintf("<tr><td %v>%v</td><td %v>%v</td><td %v>%v℃</td><td %v>%v</td></tr>\n",
+				Style1, now.Format("15:04"), Style1, city, Style1, item.Tem, Style1, item.Wea)
+			table += fmt.Sprintf("<tr><td %v>%v</td><td %v>%v</td><td %v>%v~%v℃</td><td %v>%v</td></tr>\n",
+				Style1, item.Date[5:], Style1, item.Week, Style1, item.Tem2, item.Tem1, Style1, item.Wea)
 			fmt.Printf("%v	%v	%v℃	%v\n", now.Format("15:04"), city, item.Tem, item.Wea)
-			fmt.Printf("%v	%v	%v~%v℃	%v\n", item.Date[5:], item.Week, item.Tem2, item.Tem1, item.Wea)
 			fmt.Printf("--------------------------------\n")
+			fmt.Printf("%v	%v	%v~%v℃	%v\n", item.Date[5:], item.Week, item.Tem2, item.Tem1, item.Wea)
 			for _, hour := range item.Hours {
 				if ShowHour(hour.Hours) {
-					table += fmt.Sprintf("<tr>%v%v</td>%v%v℃</td>%v%v</td>%v%v%v</td></tr>\n", TD2, hour.Hours, TD2, hour.Tem, TD2, hour.Wea, TD2, hour.Win, hour.WinSpeed)
+					table += fmt.Sprintf("<tr><td %v>%v</td><td %v>%v℃</td><td %v>%v</td><td %v>%v%v</td></tr>\n",
+						Style2, hour.Hours, Style2, hour.Tem, Style2, hour.Wea, Style2, hour.Win, hour.WinSpeed)
 					fmt.Printf("%v	%v℃	%v	%v%v\n", hour.Hours, hour.Tem, hour.Wea, hour.Win, hour.WinSpeed)
 				}
 			}
 			fmt.Printf("--------------------------------\n")
 		} else {
-			table += fmt.Sprintf("<tr>%v%v</td>%v%v</td>%v%v~%v℃</td>%v%v</td></tr>\n", TD, item.Date[5:], TD, item.Week, TD, item.Tem2, item.Tem1, TD, item.Wea)
+			table += fmt.Sprintf("<tr><td %v>%v</td><td %v>%v</td><td %v>%v~%v℃</td><td %v>%v</td></tr>\n",
+				Style1, item.Date[5:], Style1, item.Week, Style1, item.Tem2, item.Tem1, Style1, item.Wea)
 			fmt.Printf("%v	%v	%v~%v℃	%v\n", item.Date[5:], item.Week, item.Tem2, item.Tem1, item.Wea)
 		}
 	}
